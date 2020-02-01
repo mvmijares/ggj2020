d24f6e66e6064e9c055d43714e9d628d9a0a547d
Michael Vincent Mijares <mvmijares@users.noreply.github.com>
2020-02-01 14:14:34 -0500

    Implemented player movement and camera control. Working on object pick up / placement

---
+++ GlobalGameJam2020/Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 2f;
+    public bool keyboard = true;
+    private void Awake()
+    {
+        
+    }
+    private void Update()
+    {
+        PlayerInput();
+    }
+
+    private void PlayerInput()
+    {
+        if (keyboard)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.Translate(Vector3.forward * Time.deltaTime * speed);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.Translate(-Vector3.forward * Time.deltaTime * speed);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Translate(Vector3.right* Time.deltaTime * speed);
+            }
+            if (Input.GetKey(KeyCode.A)){
+                transform.Translate(-Vector3.right * Time.deltaTime * speed);
+            }
+        }
+    }
+}
---
+++ GlobalGameJam2020/Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    private float mouseX;
+    private float mouseY;
+    private float xRotation = 0;
+
+    public float camera_sensitivity;
+    private Transform player;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        player = transform.parent; //Assuming the parent is always the playerss
+    }
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseX = Input.GetAxis("Mouse X") * camera_sensitivity * Time.deltaTime;
+        mouseY = Input.GetAxisRaw("Mouse Y") * camera_sensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        player.Rotate(Vector3.up * mouseX);
+        // rotate the player on the x axis
+        //transform.localRotation = Quaternion.Euler(new Vector3(-mouseX, 0, 0));
+        //rotate the camera on the y axis
+    }
+}
