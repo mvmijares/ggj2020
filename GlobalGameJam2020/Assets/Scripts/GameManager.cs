a7da7dc5eb6cabf1066c4584403e524f9e0d8675
Michael Vincent Mijares <mvmijares@users.noreply.github.com>
2020-02-01 15:58:23 -0500

    Added object pickup. Uses raycast to detect item, and creates a duplicate object at the tip of the hand.

---
+++ GlobalGameJam2020/Assets/Scripts/GameManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region Data
+
+    public static GameManager instance = null; //Singleton
+    private int score;
+    private int timer;
+    public int maxTime;
+
+    #endregion
+    /* Will change this layer once, scene transitioning is implemented
+     * 
+     */
+    public Transform playerCamera;
+    public Transform player;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    public PlayerCamera GetPlayerCamera()
+    {
+        if (playerCamera)
+            return playerCamera.GetComponent<PlayerCamera>();
+        else
+        {
+            Debug.Log("Player camera script component not found");
+            return null;
+        }
+    }
+
+    public Player GetPlayer()
+    {
+        if (player)
+        {
+            return player.GetComponent<Player>();
+        }
+        else
+        {
+            Debug.Log("Player script component not found.");
+            return null;
+        }
+    }
+    private void Update()
+    {
+        
+    }
+}
