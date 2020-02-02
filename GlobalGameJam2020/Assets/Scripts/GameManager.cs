8e4e19fc31bbe1ff112eff03f95f5dbc4b4fa690
Michael Vincent Mijares <mvmijares@users.noreply.github.com>
2020-02-01 23:04:19 -0500

    Restarted development branch to a working backup

--- GlobalGameJam2020/Assets/Scripts/GameManager.cs
+++ GlobalGameJam2020/Assets/Scripts/GameManager.cs
@@ -1,14 +1,26 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
+public enum SceneState
+{
+    None, Menu, Help, Game, End
+}
 public class GameManager : MonoBehaviour
 {
     #region Data
-
+    public SceneState sceneState;
     public static GameManager instance = null; //Singleton
     private int score = 0;
-    private float imer;
+
+    private float startTimer = 0.0f;
+    public float startTime = 3f;
+    public bool start = false;
+
+    private float gameTimer = 0.0f;
     public float maxTime;
 
     [Tooltip("Time it takes for item to get destroyed after being used")]
@@ -20,6 +32,11 @@ public class GameManager : MonoBehaviour
     public Transform playerCamera;
     public Transform player;
 
+    #region User Interface
+
+    private Transform scoreTransform;
+
+    #endregion
     private void Awake()
     {
         if (instance == null)
@@ -31,6 +48,61 @@ public class GameManager : MonoBehaviour
         {
             Destroy(this.gameObject);
         }
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        switch (scene.name)
+        {
+            case "Development":
+                {
+                    OnDevelopmentScene();
+                    break;
+                }
+            case "Game":
+                {
+                    OnGameScene();
+                    break;
+                }
+        }
+    }
+
+    private void OnGameScene()
+    {
+
+    }
+
+    private void OnDevelopmentScene()
+    {
+        sceneState = SceneState.Game;
+        GameObject score = GameObject.FindGameObjectWithTag("Score Text");
+
+        if (score)
+            scoreTransform = score.transform;
+
+        if (!start)
+        {
+            startTimer += Time.deltaTime;
+            if (startTimer >= startTime)
+            {
+                start = true;
+                startTimer = 0.0f;
+            }
+        }
+        else
+        {
+            gameTimer += Time.deltaTime;
+            if (gameTimer >= maxTime)
+            {
+                Debug.Log("Game has finished!!");
+            }
+        }
+       
     }
     public PlayerCamera GetPlayerCamera()
     {
@@ -62,6 +134,12 @@ public class GameManager : MonoBehaviour
     }
     private void Update()
     {
-        Debug.Log("Score is " + score);
+        if (sceneState == SceneState.Game)
+        {
+            if(scoreTransform)
+                scoreTransform.GetChild(0).GetComponent<Text>().text = score.ToString();
+
+        }
+
     }
 }
---
+++ GlobalGameJam2020/Assets/Scripts/ShoppingCart.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShoppingCart : MonoBehaviour
+{
+    #region Data
+    GameManager gm_instance;
+    public int maxNumItems;
+    bool isFull;
+    List<Item> itemList;
+    Queue<ItemType> itemQueue;
+    public Transform item;
+
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        gm_instance = GameManager.instance;
+
+        itemQueue = new Queue<ItemType>();
+        itemList = new List<Item>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gm_instance.start)
+        {
+            if (!isFull)
+            {
+                SpawnNewItem();
+            }
+        }
+    }
+
+    private void SpawnNewItem()
+    {
+        if(itemList.Count == maxNumItems)
+        {
+            isFull = true;
+            return;
+        }
+        else
+        {
+            ItemType type = RandomizeNewItem();
+
+            switch (type)
+            {
+                case ItemType.Steak:
+                {
+
+                    break;
+                }
+            }
+        }
+    }
+
+    private ItemType RandomizeNewItem()
+    {
+        ItemType newType = (ItemType)UnityEngine.Random.Range(0, Enum.GetNames(typeof(ItemType)).Length);
+
+        return newType;
+    }
+}
